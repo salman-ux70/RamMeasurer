53cff1a0030181456936a4628426a3c287493562
Salman <nazeersalman978@gmail.com>
2025-02-09 13:11:01 +0500

    notification permission added.

--- app/src/main/java/com/assignment/rammeasurer/service/RamMonitorService.kt
+++ app/src/main/java/com/assignment/rammeasurer/service/RamMonitorService.kt
@@ -76,7 +76,7 @@ class RamMonitorService : Service() {
                 if (!isMonitoring) return
                 sendRamUsageToClient()
                 saveRamUsageToDatabase()
-                handler.postDelayed(this, /*5 * */10 * 1000) // Repeat every 5 minutes
+                handler.postDelayed(this, 5 * 60 * 1000) // Repeat every 5 minutes
             }
         }, 0)
     }
